078645ee253cedf14501bc3a447793a162f1c3a1
ali ahmad ali <ali.ali792wd@gmail.con>
2020-12-04 11:06:38 +0200

    initialization

---
+++ config/ldog.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'domain'=> env('DOMAIN','ldog.test'),
+    'graph_stores'=> [
+        'open'=> [
+            'driver'=> env('GS_OPEN_DRIVER','graph_db'),
+            'host'=> env('GS_OPEN_HOST','http://10.0.2.2:7200'),
+            'repository'=> env('GS_OPEN_REPOSITORY','open'),
+            'username'=> env('GS_OPEN_USERNAME','admin'),
+            'password'=> env('GS_OPEN_PASSWORD','root'),
+        ],
+        'secure'=> [
+            'driver'=> env('GS_SECURE_DRIVER','graph_db'),
+            'host'=> env('GS_SECURE_HOST','http://10.0.2.2:7200'),
+            'repository'=> env('GS_SECURE_REPOSITORY','secure'),
+            'username'=> env('GS_SECURE_USERNAME','admin'),
+            'password'=> env('GS_SECURE_PASSWORD','root'),
+        ]
+    ],
+    'reconciliation'=> [
+        'driver'=> 'graphdb_lucene',
+        'index'=> 'reconcilation',
+    ],
+    'storage'=>[
+        'disk'=>'local',
+        'directories'=>[
+            'root'=>'ldog',
+            'conversions'=>'conversions',
+        ],
+    ],
+    'silk'=>[
+        'path'=>'D:\Laravel Packages\Binaries\silk\silk-3.2.0\silk-tools\silk-singlemachine\target\scala-2.11\silk.jar',
+    ],
+];
