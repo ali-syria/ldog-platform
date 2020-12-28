24c88a266c6b3e25c95a8f8dff1a3b632446d2ca
ali ahmad ali <ali.ali792wd@gmail.con>
2020-12-28 16:28:39 +0200

    wip

--- app/Http/Livewire/Admin/Reconciliation.php
+++ app/Http/Livewire/Admin/Reconciliation.php
@@ -3,12 +3,16 @@
 namespace App\Http\Livewire\Admin;
 
 use AliSyria\LDOG\Facades\GS;
+use AliSyria\LDOG\Facades\REC;
 use AliSyria\LDOG\GraphStore\ResourceLabel;
+use AliSyria\LDOG\PublishingPipeline\Predicate;
 use AliSyria\LDOG\PublishingPipeline\PublishingPipeline;
 use AliSyria\LDOG\PublishingPipeline\TermResourceMapping;
 use AliSyria\LDOG\UriBuilder\UriBuilder;
 use AliSyria\LDOG\Utilities\LdogTypes\TermResourceMatchType;
 use App\Actions\ReconciliationAction;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 use Livewire\Component;
 
@@ -17,21 +21,41 @@ use Livewire\Component;
  */
 class Reconciliation extends Component
 {
+    public array $mappings=[];
     public string $conversionId='';
+    public string $cacheBaseKey='';
+    public string $cacheIsInitializedKey='';
+    public string $cacheFullMatchKey='';
+    public string $cachePartialMatchKey='';
 
     public function mount(string $conversion)
     {
         $this->conversionId=$conversion;
+        $this->cacheBaseKey="reconciliation.{$this->pipeline->id}";
+        $this->cacheIsInitializedKey=$this->cacheBaseKey.".is_initialized";
+        $this->cacheFullMatchKey=$this->cacheBaseKey.".full_match";
+        $this->cachePartialMatchKey=$this->cacheBaseKey.".partial_match";
     }
-    public function handle(ReconciliationAction $action)
+    public function initialize()
     {
+        if(Cache::get($this->cacheIsInitializedKey))
+        {
+            return;
+        }
+
         $termResourceMappings=[];
+        $partialMatchColumnValues=[];
+
         $fullMatchType=TermResourceMatchType::find(UriBuilder::PREFIX_CONVERSION.TermResourceMatchType::FullMatch);
+
         foreach ($predicate=$this->shapeObjectPredicates as $shapeObjectPredicate)
         {
             $columnName=$this->pipeline->getCsvColumnNameForPredicate($shapeObjectPredicate);
             $columnValues=$this->pipeline->getCsvDistinctColumnValues($columnName);
-            $resources=$this->pipeline->getShapeObjectPredicateClassResources($shapeObjectPredicate);
+            $resources=$this->shapeObjectPredicateClassResources
+                ->where('predicate',$shapeObjectPredicate->uri)
+                ->first()
+                ->resources;
             foreach ($columnValues as $columnValue)
             {
                 $targetResource=$resources->filter(function(ResourceLabel $resourceLabel,$key)use($columnValue){
@@ -41,16 +65,103 @@ class Reconciliation extends Component
                 {
                     $termResourceMappings[]=new TermResourceMapping($shapeObjectPredicate->uri,$columnValue,$targetResource->uri,$fullMatchType);
                 }
+                else
+                {
+                    $partialMatchColumnValues[]=(object)[
+                        'predicate'=>$shapeObjectPredicate->uri,
+                        'column_value'=>$columnValue
+                    ];
+                }
             }
         }
+        $this->isInitialized=true;
+        Cache::remember($this->cacheIsInitializedKey,3600,fn()=>true);
+        Cache::remember($this->cacheFullMatchKey,3600,fn()=>collect($termResourceMappings));
+        Cache::remember($this->cachePartialMatchKey,3600,fn()=>collect($partialMatchColumnValues));
+    }
+    public function resetMappings()
+    {
+        $mappings=[];
+        foreach ($this->predicates as $predicate)
+        {
+            foreach($this->partialMatchColumnValues->where('predicate',$predicate->uri) as $partialMatchObject)
+            {
+                $mappings[base64_encode($predicate->uri)][base64_encode($partialMatchObject->column_value)]=null;
+            }
+        }
+
+        return $mappings;
+    }
+    public function handle(ReconciliationAction $action)
+    {
+        $termResourceMappings=$this->extractFullTermResourcesMappings()
+            ->merge($this->extractPartialTermResourcesMappings());
+
         $action->execute($this->pipeline,collect($termResourceMappings));
+
         return redirect()->route('admin.pipeline.validation',[
             $this->conversionId
         ]);
     }
+    private function extractPartialTermResourcesMappings():Collection
+    {
+        $partialMatchType=TermResourceMatchType::find(UriBuilder::PREFIX_CONVERSION.TermResourceMatchType::PartialMatch);
+
+        $results=[];
+        foreach ($this->mappings as $predicate=>$mappings)
+        {
+            foreach ($mappings as $columnValue=>$resourceUri)
+            {
+                $results[]=new TermResourceMapping(base64_decode($predicate),base64_decode($columnValue),$resourceUri,$partialMatchType);
+            }
+        }
+
+        return collect($results);
+    }
+    private function extractFullTermResourcesMappings():Collection
+    {
+        return Cache::get($this->cacheFullMatchKey);
+    }
+    public function getIsInitializedProperty()
+    {
+        return Cache::get($this->cacheIsInitializedKey);
+    }
+    public function getPartialMatchColumnValuesProperty()
+    {
+        return Cache::get($this->cachePartialMatchKey);
+    }
+    public function search($columnValue,Predicate $predicate):Collection
+    {
+        $results=REC::search($columnValue,$predicate->objectClassUri);
+        if($results->isEmpty())
+        {
+            $results=$this->shapeObjectPredicateClassResources
+                ->where('predicate',$predicate->uri)
+                ->first()
+                ->resources;
+        }
+
+        return $results;
+    }
+    public function getShapeObjectPredicateClassResourcesProperty():Collection
+    {
+        return Cache::remember($this->cacheBaseKey."shapeObjectPredicateClassResources",3600,function(){
+            $reults=[];
+            foreach ($predicate=$this->shapeObjectPredicates as $shapeObjectPredicate)
+            {
+                $reults[]=(object)[
+                    'predicate'=>$shapeObjectPredicate->uri,'resources'=>$this->pipeline->getShapeObjectPredicateClassResources($shapeObjectPredicate)
+                ];
+            }
+
+            return collect($reults);
+        });
+    }
     public function getShapeObjectPredicatesProperty()
     {
-        return $this->pipeline->getShapeObjectPredicates();
+        return Cache::remember($this->cacheBaseKey."shapeObjectPredicates",3600,function(){
+            return $this->pipeline->getShapeObjectPredicates();
+        });
     }
     public function getPipelineProperty()
     {
@@ -58,11 +169,15 @@ class Reconciliation extends Component
     }
     public function getCsvColumnNamesProperty()
     {
-        return $this->pipeline->getCsvColumnNames();
+        return Cache::remember($this->cacheBaseKey."csvColumnNames",3600,function(){
+            return $this->pipeline->getCsvColumnNames();
+        });
     }
     public function getPredicatesProperty()
     {
-        return $this->pipeline->getShapePredicates()->sortBy('order');
+        return Cache::remember($this->cacheBaseKey."predicates",3600,function(){
+            return $this->pipeline->getShapePredicates()->sortBy('order');
+        });
     }
     public function render()
     {
--- resources/views/livewire/admin/reconciliation.blade.php
+++ resources/views/livewire/admin/reconciliation.blade.php
@@ -1,41 +1,59 @@
-<div>
+<div wire:init="initialize">
+    <span wire:loading wire:target="initialize">Loading</span>
 
-{{--    @foreach($this->shapeObjectPredicates as $objectPredicate)--}}
-{{--        <div class="mb-8">--}}
-{{--            <h2 class="font-bold text-lg capitalize">{{ $objectPredicate->name }}</h2>--}}
-{{--            <p class="text-gray-400">--}}
-{{--                {{ $objectPredicate->description }}--}}
-{{--            </p>--}}
-{{--            <table class="table-fixed border-collapse w-1/2 mt-2">--}}
-{{--                <thead>--}}
-{{--                <tr>--}}
-{{--                    <th class="w-1/3 p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Value</th>--}}
-{{--                    <th class="w/2/3 p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Term</th>--}}
-{{--                </tr>--}}
-{{--                </thead>--}}
-{{--                <tbody>--}}
-{{--                @foreach($this->predicates as $predicate)--}}
-{{--                    <tr  class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">--}}
-{{--                        <td  x-data="{ tooltip: false }" x-on:mouseover="tooltip = true" x-on:mouseleave="tooltip = false" class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">--}}
-{{--                            <div class="relative" x-cloak x-show.transition.origin.top="tooltip">--}}
-{{--                                <div class="absolute top-0 z-10 w-32 p-2 -mt-1 text-sm leading-tight text-white transform  -translate-y-full bg-orange-500 rounded-lg shadow-lg">--}}
-{{--                                    {{ $predicate->description }}--}}
-{{--                                </div>--}}
-{{--                                <svg class="absolute z-10 w-6 h-6 text-orange-500 transform -translate-y-3 fill-current stroke-current" width="8" height="8">--}}
-{{--                                    <rect x="12" y="-10" width="8" height="8" transform="rotate(45)" />--}}
-{{--                                </svg>--}}
-{{--                            </div>--}}
-{{--                            {{ $predicate->name }}--}}
-{{--                        </td>--}}
-{{--                        <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">--}}
-{{--                            <x-form.select name="mappings.{{ base64_encode($predicate->uri) }}" :items="$this->csv_column_names" :use-text-as-key="true" required/>--}}
-{{--                        </td>--}}
-{{--                    </tr>--}}
-{{--                @endforeach--}}
-{{--                </tbody>--}}
-{{--            </table>--}}
-{{--        </div>--}}
-{{--    @endforeach--}}
+    <div class="grid grid-cols-2 gap-4" x-data="{selectedPredicate:null}">
+        <div>
+            @if($this->isInitialized)
+                @foreach($this->shapeObjectPredicates as $objectPredicate)
+                    @if($this->partialMatchColumnValues->where('predicate',$objectPredicate->uri)->isEmpty())
+                        @continue
+                    @endif
+                    <div class="mb-8">
+                        <h2 class="font-bold text-lg capitalize">{{ $objectPredicate->name }}</h2>
+                        <p class="text-gray-400">
+                            {{ $objectPredicate->description }}
+                        </p>
+                        <table class="table-fixed border-collapse w-full mt-2">
+                            <thead>
+                            <tr>
+                                <th class="w-1/3 p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Value</th>
+                                <th class="w/2/3 p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Term</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($this->partialMatchColumnValues->where('predicate',$objectPredicate->uri) as $partialMatchObject)
+                                <tr  class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
+                                    <td  class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                                        {{ $partialMatchObject->column_value }}
+                                    </td>
+                                    <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                                        <x-form.select name="mappings.{{ base64_encode($partialMatchObject->predicate) }}.{{ base64_encode($partialMatchObject->column_value) }}" :items="$this->search($partialMatchObject->column_value,$objectPredicate)" key="uri" value="label" required x-on:change="$dispatch('show-resource',$event.target.value)" />
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                @endforeach
+            @endif
+        </div>
+        <div class="relative">
+
+        </div>
+
+    </div>
+    <div x-ref="resourceViewer" class="bg-indigo-200 p-4 w-md" style="position: absolute;bottom:50px;" x-data x-on:show-resource.window="
+            if(!$event.detail)
+            {
+                $refs.resourceViewer.innerHTML='';
+            }
+            fetch($event.detail)
+                .then(response => response.text())
+                .then(html => { $refs.resourceViewer.innerHTML = html })
+">
+
+    </div>
+
     <div class="form-group mt-8">
         <button type="submit" wire:click.prevent="handle" wire:loading.attr="disabled" class="px-4 py-2 font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple text-sm" style="background: #047481">
             <div wire:loading.remove wire:target="handle">
