708b2d2ee3aebefcb3479250c76195c8204bfa71
ali ahmad ali <ali.ali792wd@gmail.con>
2021-05-20 09:55:08 +0300

    wip

--- app/Actions/InitializePublishingPipelineAction.php
+++ app/Actions/InitializePublishingPipelineAction.php
@@ -5,6 +5,7 @@ namespace App\Actions;
 use AliSyria\LDOG\Contracts\TemplateBuilder\DataTemplate;
 use AliSyria\LDOG\PublishingPipeline\PublishingPipeline;
 use AliSyria\LDOG\TemplateBuilder\DataCollectionTemplate;
+use AliSyria\LDOG\TemplateBuilder\ReportTemplate;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -29,6 +30,10 @@ class InitializePublishingPipelineAction
     public function execute(string $dataTemplateUri,UploadedFile $dataFile):PublishingPipeline
     {
         $dataTemplate=DataCollectionTemplate::retrieve($dataTemplateUri);
+        if(is_null($dataTemplate))
+        {
+            $dataTemplate=ReportTemplate::retrieve($dataTemplateUri);
+        }
         $fileName=Str::random().'.'.$dataFile->getClientOriginalExtension();
         $path=$dataFile->storeAs('data/csv',$fileName,'public');
         $fullPath=Storage::disk('public')->path($path);
